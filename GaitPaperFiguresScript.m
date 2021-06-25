597d7ea922d5b4167af2be7794e6021adb41475e
Jacob Zavatone-Veth <jzavatoneveth@users.noreply.github.com>
2021-06-25 11:16:47 -0400

    Move code for Figure 7 to main script

--- GaitPaperFiguresScript.m
+++ GaitPaperFiguresScript.m
@@ -374,6 +374,44 @@ PlotSwingStanceStatisticsConditionedOnCentroidKinematics( newData, vfInterval );
 % Step length & direction conditioned on centroid kinematics
 PlotAverageStepLengthAndDirection( newData, vfInterval );
 
+%% Figure 7: Timing of yaw rate extrema
+
+% Set minimum forward speed for yaw extremum detection
+vfMin = 0.5;
+
+% Set number of permutations for Monte Carlo
+nperm = 1e5;
+
+% Set edges for discretization
+phiBinEdges = (0:1/180:1)';
+
+% Select whether to plot bootstrapped confidence intervals on PDFs
+plotCI = true;
+
+% Limb position distributions at all points
+PlotLimbPositionDistributionsAtAllPoints(newData);
+
+% Limb position distributions at yaw extrema
+PlotLimbPositionDistributionsAtTimeOfTurn(newData);
+
+% Phase distributions a all points
+[ fPDFnull, fCDFnull, xq, nnull, xnull, idnull ]  = PlotPhaseDistributionsAtAllPoints(newData, phiBinEdges, plotCI);
+
+% Phase distributions at yaw extrema
+[ fPDFturn, fCDFturn, xq, nturn, xturn, idturn ] = PlotPhaseDistributionsAtTimeOfTurn(newData, phiBinEdges, plotCI);
+
+% Perform Kuiper tests
+p = nan(6,1);
+pci = nan(6,2);
+vstat = nan(6,1);
+for ind = 1:6
+    
+    countsNull = accumarray([discretize(xnull(:,ind), phiBinEdges), idnull], 1);
+    countsTurn = accumarray([discretize(xturn(:,ind), phiBinEdges), idturn], 1);
+    
+    [p(ind), pci(ind,:), vstat(ind), ~] = kuiper2permtest(countsNull,countsTurn, nperm);
+end
+
 
 %% Figure 8: Experimental perturbations of walking speed modulate stance duration.
 
